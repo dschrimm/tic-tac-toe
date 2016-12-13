fd1da0c4e56e20cd7827b60f0092f123e041a3e9
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-12-13 13:33:52 -0800

    WIP: initialize player with identifier

--- spec/tic_tac_toe.spec.js
+++ spec/tic_tac_toe.spec.js
@@ -1,9 +1,20 @@
 import TicTacToe from 'tic_tac_toe';
+import Player from 'tic_tac_toe';
 
 describe('TicTacToe', function() {
   describe('the truth', function() {
     it('should return true', function() {
       expect(true).toEqual(true);
+    });
+  });
+});
+
+describe('Player', function() {
+  var testPlayer = new Player();
+
+  describe('player', function() {
+    it('should have a marker', function() {
+      expect(testPlayer.marker).toEqual(1);
     });
   });
 });
--- src/tic_tac_toe.js
+++ src/tic_tac_toe.js
@@ -1,3 +1,10 @@
-var TicTacToe = {};
+var TicTacToe = function(){
+
+};
+
+var Player = function(){};
+// var Player = function(num) {
+//   this.marker = num;
+// };
 
 export default TicTacToe;
