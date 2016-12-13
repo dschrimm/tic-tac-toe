008c056da6862c55acca1eea982c9cfb618c9fb5
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-12-13 15:32:52 -0800

    add game initialize and place marker tests and functionalities

--- spec/tic_tac_toe.spec.js
+++ spec/tic_tac_toe.spec.js
@@ -1,9 +1,38 @@
 import TicTacToe from 'tic_tac_toe';
+import Board from 'board';
+import Player from 'player';
 
 describe('TicTacToe', function() {
-  describe('the truth', function() {
-    it('should return true', function() {
-      expect(true).toEqual(true);
+  var testGame = new TicTacToe();
+
+  describe('ticTacToe', function() {
+    it('should start the turn count at 0', function() {
+      expect(testGame.totalTurns).toEqual(0);
+    });
+
+    it('should initialize a new game with a new board', function() {
+      expect(testGame.board instanceof Board).toEqual(true);
+    });
+
+    it('should start with an empty board', function() {
+      expect(testGame.board.playingField).toEqual([[0,0,0],[0,0,0],[0,0,0]]);
+    });
+
+    it('should start with two players', function() {
+      expect(testGame.players.length).toEqual(2);
+    });
+
+    it('should have player objects', function() {
+      expect(testGame.players[0] instanceof Player).toEqual(true);
+      expect(testGame.players[1] instanceof Player).toEqual(true);
+    });
+  });
+
+  describe('placeMarker', function() {
+    it('should place appropriate player\'s marker on designated space', function() {
+      expect(testGame.board.emptySpace(0,1)).toEqual(true);
+      testGame.placeMarker(0,1,2);
+      expect(testGame.board.emptySpace(0,1)).toEqual(false);
     });
   });
 });
--- src/tic_tac_toe.js
+++ src/tic_tac_toe.js
@@ -1,5 +1,15 @@
-var TicTacToe = function(){
+import Board from 'board';
+import Player from 'player';
 
+var TicTacToe = function(){
+  this.MAX_TURNS = 9;
+  this.totalTurns = 0;
+  this.board = new Board();
+  this.players = [new Player(1), new Player(2)];
+};
+
+TicTacToe.prototype.placeMarker = function(row, column, player) {
+  this.board.playingField[row][column] = player;
 };
 
 
