b8b848aa6baf4918a674d1a8bcaf82a91ef47598
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-12-19 15:05:34 -0800

    Convert player to Backbone model

--- src/app/models/player.js
+++ src/app/models/player.js
@@ -1,6 +1,10 @@
-var Player = function(num) {
-  this.marker = num;
-};
+import Backbone from 'backbone';
+
+const Player = Backbone.Model.extend({
+  initialize: function(num) {
+    this.marker = num;
+  }
+});
 
 
 export default Player;
