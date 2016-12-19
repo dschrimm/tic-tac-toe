c7a27c1b6244bcfdb7f16dc44471f2c83ad28b18
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-12-19 09:08:50 -0800

    attempt at making board have events

--- src/app/views/board_view.js
+++ src/app/views/board_view.js
@@ -3,7 +3,24 @@
 // import SpaceView from 'app/views/space_view';
 
 var BoardView = Backbone.View.extend({
+  initialize: function() {
+    console.log("BoardView created");
+    // this.template = _.template(Backbone.$('#tmpl-trip-card').html());
+  },
 
+  events: {
+    'click': 'onClick'
+  },
+
+  onClick: function(e) {
+    console.log('on click');
+    this.trigger('select', this);
+
+    // We return false to tell jQuery not to run any more event handlers.
+    // Otherwise, it would run the 'click' event handler on RolodexView
+    // as well.
+    return false;
+  },
 });
 
 // export default BoardView;
