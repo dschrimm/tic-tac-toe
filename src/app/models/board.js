5af368c2f5692720dd52820996ced7b2a29b4026
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-12-19 15:04:10 -0800

    Convert board to Backbone model

--- src/app/models/board.js
+++ src/app/models/board.js
@@ -1,13 +1,17 @@
-var Board = function() {
-  this.playingField = [[0,0,0],[0,0,0],[0,0,0]];
-};
+import Backbone from 'backbone';
 
-Board.prototype.emptySpace = function(row, column) {
-  if (this.playingField[row][column] === 0) {
-    return true;
-  } else {
-    return false;
+const Board = Backbone.Model.extend({
+  initialize: function() {
+    this.playingField = [[0,0,0],[0,0,0],[0,0,0]];
+  },
+
+  emptySpace: function(row, column) {
+    if (this.playingField[row][column] === 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
-};
+});
 
 export default Board;
